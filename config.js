4dce6d1d57dd1632610f136869c231e2b5b1d4d8
freedomnew2009-collab <freedomnew2009@gmail.com>
2026-08-09 19:09:40 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,6 +3,6 @@
 //  - SUPABASE_URL      = Project URL (เช่น https://xxxx.supabase.co)
 //  - SUPABASE_ANON_KEY = anon public key หรือ publishable key (sb_publishable_...)
 window.APP_CONFIG = {
-  SUPABASE_URL: "",
-  SUPABASE_ANON_KEY: "",
+  SUPABASE_URL: "https://jtswvypgdtwktbsmmklw.supabase.co",
+  SUPABASE_ANON_KEY: "sb_publishable_NopJdR6B4bapR_hywVzKZA_izRV5JhE",
 };
